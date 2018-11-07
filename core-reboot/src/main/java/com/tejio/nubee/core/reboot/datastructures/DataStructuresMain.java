b0b052f5ff19451aeaff4275760401ff0b6fd084
teja <teja@192.168.0.4>
2018-11-06 18:54:07 -0800

    initial rebase

---
+++ core-reboot/src/main/java/com/tejio/nubee/core/reboot/datastructures/DataStructuresMain.java
@@ -0,0 +1,10 @@
+package com.tejio.nubee.core.reboot.datastructures;
+
+public class DataStructuresMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

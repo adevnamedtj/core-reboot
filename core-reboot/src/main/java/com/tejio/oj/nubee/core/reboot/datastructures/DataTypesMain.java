259d4a19cfa6d9123d230a2be4c3a32bf11ff6ab
teja <teja@192.168.0.4>
2018-11-06 19:45:50 -0800

    longMax corrected

---
+++ core-reboot/src/main/java/com/tejio/oj/nubee/core/reboot/datastructures/DataTypesMain.java
@@ -0,0 +1,54 @@
+package com.tejio.oj.nubee.core.reboot.datastructures;
+
+public class DataTypesMain {
+
+	public static void main(String[] args) {
+	
+    // PREMATIVE DATA TYPES
+		//byte is 8 bits -> -2^7 to 2^7-1
+		//used to store smaller integer
+		//Default value is 0
+		byte byteMin = -128;
+		byte byteMax = 127;
+		
+		//short is 16 bits -> -2^15 to 2^15-1
+		//Default is 0
+		short shortMin = -32_768;
+		short shaortMax = 32_767;
+		
+		//int is 32 bits -> -2^31 to 2^31-1
+		//Default is 0
+		int intMin = -2_147_483_648;
+		int intMax = 2_147_483_647;
+		
+		
+		//long 64 bit -> -2^63 to 2^63-1
+		//Default 0L
+		//used when range is wider than basic int
+		long longMin = -9_223_372_036_854_775_808L;
+		long longMax = 9_223_372_036_854_775_807L;
+		
+		
+		//float 32 bit
+		//Default 0.0f
+		float floatExample = 277.7f;
+		
+		//double 64 bit
+		//Default 0.0d
+		double doubleExample = 27777.77777d;
+		
+		//boolean 1 bit
+		boolean booleanExample = true;
+		
+		//char 16 bit unicode
+		char chatMin = '\u0000';
+		char chatMax = '\uffff';
+		
+	// REFRENCE DATA TYPES	
+		
+		//objects and other collections
+		
+	
+	}
+
+}
